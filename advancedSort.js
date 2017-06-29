6b05095d1badb3fde2e137c4cb20c9328c5f0c00
Zola <zolamcadie@gmail.com>
2017-06-29 20:08:06 +0000

    sorts students alphabetically by name giving preference to older students

---
+++ advancedSort.js
@@ -0,0 +1,11 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b) {
+  if (a.name === b.name) return b.age - a.age;
+  return a.name > b.name;
+});
---
+++ sort.js
@@ -0,0 +1,5 @@
+var array = [10, 2, 5, 1, 9];
+
+array.sort(function(a, b) {
+  return a - b
+});
